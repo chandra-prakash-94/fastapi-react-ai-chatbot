6621d49a2df84bd59d8f97fa27796cfba7998313
Chandra Prakash <29017226+chandra-prakash-94@users.noreply.github.com>
2026-02-24 01:13:42 +0000

    feat: add langchain version

--- chatbot-backend/main.py
+++ chatbot-backend/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
-import google.generativeai as genai
+from langchain.chat_models import init_chat_model
 import os
 from dotenv import load_dotenv
 from fastapi.middleware.cors import CORSMiddleware
@@ -12,11 +12,9 @@ load_dotenv()  # Load environment variables from .env file
 if "GOOGLE_API_KEY" not in os.environ:
     raise EnvironmentError("GOOGLE_API_KEY environment variable is not set.")
 
-genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
-
 # --- Model Initialization ---
 try:
-    model = genai.GenerativeModel('gemini-2.0-flash')
+    model = init_chat_model('google_genai:gemini-2.5-flash-lite')
 except Exception as e:
     raise RuntimeError(f"Failed to initialize Gemini model: {e}") from e
 
@@ -55,7 +53,12 @@ async def chat(chat_input: ChatInput):
     Endpoint for chatting with the AI model.
     """
     try:
-        response = model.generate_content(chat_input.user_message)
-        return {"response": response.text}
+        response = model.invoke(chat_input.user_message)
+        return {"response": response.content}
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Error generating content: {e}")
+    
+if __name__=="__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+
